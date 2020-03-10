b4ab11f08cb420a1eac2d6e8ccb889ed8e740ae5
Blake <blakewoodruff4@gmail.com>
2020-03-10 20:44:27 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -68,10 +68,6 @@ def adding_matz
 
 	programmer_hash = 
  		{
- 		  :yukihiro_matsumoto => {
-        :known_for => "Ruby",
-        :languages => ["LISP","C"]
-      },
       :grace_hopper => {
         :known_for => "COBOL",
         :languages => ["COBOL", "FORTRAN"]
@@ -85,6 +81,12 @@ def adding_matz
         :languages => ["C"]
       }
     }
+  
+  return programmer_hash[0]={
+    :yukihiro_matsumoto => {
+      :known_for => "Ruby",
+      :languages => ["LISP","C"]
+  }
   
 end
 
